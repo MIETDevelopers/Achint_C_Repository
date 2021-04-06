dea47d8607b27a6249a3b9eca85ceb46661a7132
Achint Sathoo <76390785+Achint-NOVA@users.noreply.github.com>
2021-04-06 10:36:43 +0530

    OddEven.c added

---
+++ OddEven.c
@@ -0,0 +1,14 @@
+//Author: Achint Sathoo
+//Creation Date: 03-04-21
+//Purpose: A C Program to check whether the entered number is Odd or Even.
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num); //It will scan the numbers entered by user.
+    if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+    return 0;
+}
