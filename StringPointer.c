0934dddc7754f3ffec93c10cecd2ef56a8dd2ba2
Achint Sathoo <76390785+Achint-NOVA@users.noreply.github.com>
2021-04-07 05:54:31 +0530

    StringPointer.c added

---
+++ StringPointer.c
@@ -0,0 +1,19 @@
+//Author: Achint Sathoo
+//Creation Date: 03-04-21
+//Purpose: A C Program to read the string in pointers loaction and give it as output.(Pre-Defined)
+#include <stdio.h>
+
+int main(void) {
+  char name[] = "Achint Sathoo";
+
+  printf("%c \n", *name);     	// Output will be I
+  printf("%c \n", *(name+3));	// Output will be A
+  printf("%c \n", *(name+8));   // Output will be R
+
+  char *namePtr;
+
+  namePtr = name;
+  printf("%c \n", *namePtr);     // Output will be I
+  printf("%c \n", *(namePtr+2)); // Output will be H
+  printf("%c \n", *(namePtr+7)); // Output will be E
+}
