3ac617572eb01c260138aa0fdc5d4d1a5100ca30
Achint Sathoo <76390785+Achint-NOVA@users.noreply.github.com>
2021-04-10 12:21:09 +0530

    Update 31_ReadWriteMarks.c

--- 31_ReadWriteMarks.c
+++ 31_ReadWriteMarks.c
@@ -1,10 +1,9 @@
 //Author: Achint Sathoo
 //Creation Date: 03-04-21
-//Purpose: A C program to Program for reading and writing the student marks data to files.
+//Purpose: To create a C program for reading and writing the student marks data to files.
 #include <stdio.h>
 #include <stdlib.h>
-struct s {
-	//Creating a user defined data type using structures.
+struct s {    //Making a data type that is user defined using structures.
 	char name[50];
 	int RollNo;
 	int Marks;
@@ -13,10 +12,10 @@ int main() {
 	struct s a[10],b[10];
 	FILE *fptr;
 	int i;
-	fptr=fopen("E:\\Softwares\\C_Programs\\Achint_C_Repository\\ReadWriteMarks.txt","wb"); //Opening the file from the location.
+	fptr=fopen("E:\\Softwares\\C_Programs\\Achint_C_Repository\\ReadWriteMarks.txt","wb"); //Locating the file and opening it
 	for (i=0;i<1;++i) {
 		fflush(stdin);
-		//Scanning the entered Values.
+		//Using gets and printf function scanning the entered Values.
 		printf("Enter Name: ");
 		gets(a[i].name);
 		printf("Enter Roll Number: ");
